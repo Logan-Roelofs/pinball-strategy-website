c1c68c6f214807032025bda03ce93cb26ac3f85f
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2025-02-04 08:23:03 -0600

    update sinbad

--- app/pinball/sinbad/page.tsx
+++ app/pinball/sinbad/page.tsx
@@ -6,27 +6,24 @@ const bookData = {
     {
       number: 1,
       title: "Layout",
-      summary: `Sinbad has a very simple layout that does not need explaning, the hardest shot in the game is the left orbit
-      1. Dead bounce a lot, but nudge the machine up every time you dead bounce 
-      2. The hardest and most dangerous shot in the game is the left orbit
-      3. You can cradle the ball on the leftmost and right most flippers`,
+      summary: `Sinbad has a very simple layout that does not need explaining, the hardest shot in the game is the left orbit 1. Dead bounce a lot, but nudge the machine up every time you dead bounce 2. The hardest and most dangerous shot in the game is the left orbit 3. You can cradle the ball on the leftmost and rightmost flippers. The strategy`,
       image: "/static/sinbad.png"
     },
     {
       number: 2,
       title: "The strategy",
-      summary: `Bounes are the most important thing in Sinbad. Get your bounes multipliers and dead bounce with a nudge as much as you can.`,
+      summary: `Bonuses are the most important thing in Sinbad. Get your bonus multipliers and dead bounce with a nudge as much as you can. How to Multiply Bonuses How to Multiply Bonuses`,
       image: ""
     },
     {
       number: 3,
-      title: "How to Multiply Bounes",
-      summary: "Hit the dropt targets in order of white, yellow, blue, and red to get the bounes multipliers respectively. For example, if you have all of the drop targets down except for the white one, you will have no bounes multipliers",
+      title: "How to Multiply Bonuses",
+      summary: "Hit the drop targets in order of white, yellow, blue, and red to get the bonus multipliers respectively. For example, if you have all of the drop targets down except for the white one, you will have no bonus multipliers. How to add Bonuses How to add Bonuses",
       image: "/static/sinbaddroptargets.png"
     },{
       number: 4,
-      title: "How to add Bounes",
-      summary: "Other than drop targets, you can also get bounes in 2 ways. The first way is to hit the 4 rollovers on the left side of the playfild to advance the bounes indecator in the middle of the playfild. The second way is to hit the spinner in the middle of the playfild to advance the bounes indecator. You need to advance the bounes indecator 5 times to get a bounes", 
+      title: "How to add Bonuses",
+      summary: "Other than drop targets, you can also get bonuses in 2 ways. The first way is to hit the 4 rollovers on the left side of the playfield to advance the bonus indicator in the middle of the playfield. The second way is to hit the spinner in the middle of the playfield to advance the bonus indicator. You need to advance the bonus indicator 5 times to get a bonus.", 
       image: "/static/sinbadadaddbounes.png"
     },
   ]
